e0104f19d43cf2a00a5f14d0ccc6c66a6d1e7879
freddy74 <freddygarro74@gmail.com>
2024-06-22 16:21:08 -0600

    added variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['build/*.html'],
   theme: {
+    colors: {
+      'primary': '#03595a',
+      'secondary': '#cd7f72',
+    },
+
+    fontFamily: {
+      'sans': ['"Poppins"', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
